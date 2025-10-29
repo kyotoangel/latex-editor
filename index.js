9571a4d705d37eaf5c84ad553b11fcc03cf125ed
kyotoangel <antonin.roux@insa-lyon.fr>
2025-10-29 12:01:47 +0100

    first commit (react)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,7 +11,11 @@ function createWindow() {
     },
   });
 
-  win.loadFile('index.html');
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../src/index.html'));
+  }
 }
 
 app.whenReady().then(() => {
